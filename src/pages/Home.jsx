cc1663175d1023785b11956760888089797a7b95
Malika <malika754@Malika-MacBook-Pro.local>
2023-06-03 22:19:06 +0300

    Initiated homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { AppBar, Container, Toolbar} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { useState } from 'react'
+
+
+export default function Home() {
+    return (
+        <>
+            <AppBar color="transparent">
+                <Toolbar>
+                    <IconButton size='large'>
+                        <SettingsIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <h1>Hi User!</h1>
+            </Container>
+        </>
+    );
+}
