2263d0e74516607b3ce31823f3edaeab7683a438
Andrey Bejarano <andreybejarano@gmail.com>
2023-06-04 01:57:11 +0300

    feat: implement Context Providers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,8 @@
-import { useEffect } from 'react'
 import { AppBar, Container, Toolbar} from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import SettingsIcon from '@mui/icons-material/Settings'
 
-import QuestionnairesService from '../services/Questionnaires'
-
 export default function Home() {
-    useEffect(() => {
-        async function getQuestionnaires() {
-            try {
-                const questionnaires = await QuestionnairesService.get()
-                console.log(questionnaires)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getQuestionnaires()
-    }, []) 
-
     return (
         <>
             <AppBar color="transparent">
