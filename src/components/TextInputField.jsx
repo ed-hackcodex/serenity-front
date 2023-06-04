fa0d95d3e2436bd4e0f26413d22bd86b29798220
Malika <malika754@Malika-MacBook-Pro.local>
2023-06-04 11:56:07 +0300

    Added icons to TextInputField

--- src/components/TextInputField.jsx
+++ src/components/TextInputField.jsx
@@ -1,9 +1,20 @@
-import { Box, TextField } from "@mui/material";
+import { Box, TextField, InputAdornment } from "@mui/material";
+import MicIcon from '@mui/icons-material/Mic';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function TextInputField() {
     return (
-        <Box component='form' padding={2}>
-            <TextField fullWidth></TextField>
+        <Box component='form' padding={2} sx={{ display: "flex", alignItems: "center"}}>
+            <TextField sx={{flexGrow: 1}}
+                InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <SendIcon />
+                      </InputAdornment>
+                    ),
+                  }}>
+            </TextField>
+            <MicIcon fontSize="large"/>
         </Box>
     );
 }
